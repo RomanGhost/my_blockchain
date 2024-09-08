e36d0f6b911d53c9d1438bf397f057fb09096808
Ghost <rildiarov@gmail.com>
2024-09-08 18:39:44 +0400

    result web-server

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,24 @@ use std::{
     thread,
     time::Duration,
 };
+use rust_chat_server::ThreadPool;
 
 fn main() {
-    let listener = TcpListener::bind("5.42.101.120:7878").unwrap(); // Используем локальный хост для тестирования
+    let listener = TcpListener::bind("localhost:7878").unwrap(); // Используем локальный хост для тестирования
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     // Получаем IP-адрес клиента
-    let mut client_addr_ip;
+    let client_addr_ip;
     match stream.peer_addr() {
         Ok(client_addr) => {
             client_addr_ip = client_addr.to_string();
@@ -43,13 +47,23 @@ fn handle_connection(mut stream: TcpStream) {
         }
     };
 
-    let (status_line, filename) = if http_request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "pages/index.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "pages/404.html")
+    let (status_line, filename) = match &http_request_line[..]{
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "index.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "index.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
     };
+    let filepath = format!("pages/{filename}");
+    //     if http_request_line == "GET / HTTP/1.1" {
+    //     thread::sleep(Duration::from_secs(5));
+    //     ("HTTP/1.1 200 OK", "pages/index.html")
+    // } else {
+    //     ("HTTP/1.1 404 NOT FOUND", "pages/404.html")
+    // };
 
-    let raw_content = fs::read_to_string(filename).unwrap_or_else(|e| {
+    let raw_content = fs::read_to_string(filepath).unwrap_or_else(|e| {
         println!("Ошибка при чтении файла: {}", e);
         String::new()
     });
