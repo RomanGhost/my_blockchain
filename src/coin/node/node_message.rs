ef718c6f95246361700cae954f2938fc3de012d2
Ghost <rildiarov@gmail.com>
2025-03-15 13:03:22 +0400

    fix mining node

--- src/coin/node/node_message.rs
+++ src/coin/node/node_message.rs
@@ -1,12 +1,4 @@
-use crate::coin::node::blockchain::block::Block;
 use crate::coin::node::blockchain::transaction::SerializedTransaction;
-
-pub enum BlockchainMessage{
-    ForceBlockAdd(Block),
-    BlockAdd(Block),
-    Chain(Vec<Block>),
-    ChainCheck(Vec<Block>),
-}
 
 pub enum TransactionMessage{
     AddTransaction(SerializedTransaction),
